91e852cf3c2382319abced59fb329554113944f7
Govind Deshmukh <govind.deshmukh@pentasynth.com>
2024-06-01 13:15:00 +0530

    added generate ssl bugs fix

--- generate-ssl.sh
+++ generate-ssl.sh
@@ -25,39 +25,49 @@ server {
     }
 }
 EOF
+    sudo ln -s /etc/nginx/sites-available/$1 /etc/nginx/sites-enabled/
+    sudo nginx -t
+    sudo systemctl restart nginx
+    print_section "Nginx Configuration Completed"
 }
 
 nginx_version=$(nginx -v 2>&1)
 if [ $? -eq 0 ]; then
     print_section "Nginx is installed: $nginx_version"
 else
-    print_section "Nginx is not installed"
+    print_section "Nginx is not installed . . . Installing Nginx"
     sudo apt install -y nginx
     exit 1
 fi
 
-read -p "Do you have certbot installed? (y/n) " certbot_installed
-if [ "$certbot_installed" == "y" ]; then
-    print_section "Certbot is installed"
+certbot_installed=$(certbot --version 2>&1)
+
+if [ $? -eq 0 ]; then
+    print_section "Certbot is installed or not? $certbot_installed"
 else
-    print_section "Certbot is not installed"
+    print_section "Certbot is not installed . . . Installing Certbot"
     sudo snap install certbot --classic
     exit 1
 fi
 
-read -p "Is nginx already configured? (y/n) " nginx_configured
+echo ""
+echo "============================================================"
+echo "Is nginx config already configured ?"
+read nginx_configured
 if [ "$nginx_configured" == "y" ]; then
-    print_section "Nginx is already configured"
+    print_section "is nginx config already configured ?"
+    read -p "Enter your domain name: " domain
+    read -p "Enter the port number for backend server: " backend_port
+    create_nginx_config $domain $backend_port
+    sudo certbot certonly --nginx -d $domain    
+    print_section "Generate SSL Certificates"
+    exit 1
 else
     print_section "Nginx is not configured"
+    read -p "Enter your domain name: " domain
+    read -p "Enter the port number for backend server: " backend_port
+    create_nginx_config $domain $backend_port
+    sudo certbot certonly --nginx -d $domain
+    print_section "Generate SSL Certificates"
     exit 1
 fi
-
-read -p "Enter your domain name: " domain
-read -p "Enter the port number for backend server: " backend_port
-
-create_nginx_config $domain $backend_port
-
-sudo certbot certonly --nginx -d $domain
-
-print_section "Generate SSL Certificates"
